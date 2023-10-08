c7414bcc076d28616afa93a74959e620e4feb5d6
Oleksandr Andreiev <andreyew89@gmail.com>
2023-10-08 19:13:08 +0300

    chapter 4 has been added

---
+++ ch_4/app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { PersonScore } from './PesonScore';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonScore />
+    </div>
+  );
+}
+
+export default App;
