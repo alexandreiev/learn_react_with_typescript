2b733169cc25199ee201625674c73f0f3b2c67e8
Oleksandr Andreiev <andreyew89@gmail.com>
2023-10-19 14:45:11 +0300

    chapter 6 has been added

---
+++ ch_6/myapp/src/Routes.tsx
@@ -0,0 +1,45 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ProductsPage } from './pages/ProductsPage';
+import App from './App';
+import { ProductPage } from './ProductPage';
+import { ErrorPage } from './pages/ErrorPage';
+import { HomePage } from './pages/HomePage';
+import { Suspense, lazy } from 'react';
+
+const AdminPage = lazy(() => import('./pages/AdminPage'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'products',
+        element: <ProductsPage />,
+      },
+      {
+        path: 'products/:id',
+        element: <ProductPage />,
+      },
+      {
+        path: 'admin',
+        element: (
+          <Suspense
+            fallback={<div className="text-center p-5 text-xl text-slate-600">Loading...</div>}
+          >
+            <AdminPage />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+
+export function Routes() {
+  return <RouterProvider router={router} />;
+}
---
+++ ch_6/myapp/src/pages/AdminPage.tsx
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+  return (
+    <div className="text-center p-5 text-xl">
+      <h1 className="text-xl text-slate-600">Admin Panel</h1>
+      <p className="text-base test-slate-600">You shouldn't come here ofter because I'm lazy</p>
+    </div>
+  );
+}
