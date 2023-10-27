9b41821fc1e973c096e38eb82a0efe7bbb7287c2
Oleksandr Andreiev <andreyew89@gmail.com>
2023-10-27 08:30:23 +0300

    UsingReactContext has been added to the chapter 8

---
+++ ch_8/UsingReactContext/myapp/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Header } from "./Header";
+import { Main } from "./Main";
+import { AppProvider } from "./AppContext";
+
+function App() {
+  return (
+    <div className="max-w-7xl mx-auto px-4">
+      <AppProvider>
+        <Header />
+        <Main />
+      </AppProvider>
+    </div>
+  );
+}
+
+export default App;
