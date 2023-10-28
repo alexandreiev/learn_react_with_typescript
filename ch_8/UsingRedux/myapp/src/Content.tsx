033449908c98a275e7752b7d25a5c1145cb8a89e
Oleksandr Andreiev <andreyew89@gmail.com>
2023-10-28 17:58:01 +0300

    UsingRedux has been added to the chapter 8

---
+++ ch_8/UsingRedux/myapp/src/Content.tsx
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux";
+import { RootState } from "./store/store";
+
+export function Content() {
+  const permissions = useSelector((state: RootState) => state.user.permissions);
+  if (permissions === undefined) {
+    return null;
+  }
+
+  return permissions.includes("admin") ? (
+    <p className="mt-4 text-1 text-center">
+      Some important stuff that only an admin can do
+    </p>
+  ) : (
+    <p className="mt-4 text-1 text-center">Insufficient permissions</p>
+  );
+}
